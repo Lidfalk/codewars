7b40b5a3b217436ca27866a8042a2fdfe979abcb
Hans Sjöberg <Hans Sjberg>
2016-04-21 08:52:47 +0200

    Fixed a bunch of lint warnings

--- javascript/kata.js
+++ javascript/kata.js
@@ -1,3 +1,4 @@
+
 'use strict'; // eslint-disable-line
 
 module.exports = {
@@ -27,9 +28,9 @@ module.exports = {
     return parseInt(trimmedString, 10);
   },
   isIsogram: (str) => {
-    var lowerCaseStr = str.toLowerCase();
-    var map = {};
-    for (var index = 0; index < lowerCaseStr.length; index++) {
+    const lowerCaseStr = str.toLowerCase();
+    const map = {};
+    for (let index = 0; index < lowerCaseStr.length; index++) {
       if (map.hasOwnProperty(lowerCaseStr[index])) {
         return false;
       }
@@ -37,35 +38,34 @@ module.exports = {
     }
     return true;
   },
-  GetSum: ( a, b ) => {
+  getSum: (a, b) => {
     let result = 0;
-   // loop from the min to the max argument
-   for (var index = Math.min(a, b); index <= Math.max(a, b); index++) {
-     result += index;
-   }
-   return result;
+    // loop from the min to the max argument
+    for (let index = Math.min(a, b); index <= Math.max(a, b); index++) {
+      result += index;
+    }
+    return result;
   },
   tickets: (peopleInLine) => {
-    var bills = {'25': 0, '50': 0};
+    const bills = { 25: 0, 50: 0 };
     for (let i = 0; i < peopleInLine.length; i++) {
-      if (peopleInLine[i] / 25 == 1) {
-        bills['25'] +=1;
+      if (peopleInLine[i] / 25 === 1) {
+        bills[25] += 1;
         continue;
-      } else if (peopleInLine[i] / 25 == 2) {
-        if(bills['25'] > 0)
-        {
-          bills['25'] -=1;
-          bills['50'] +=1;
+      } else if (peopleInLine[i] / 25 === 2) {
+        if (bills[25] > 0) {
+          bills[25] -= 1;
+          bills[50] += 1;
           continue;
         }
         return 'NO';
       } else {
         if (bills['50'] > 0 && bills['25'] > 0) {
-          bills['50'] -=1;
-          bills['25'] -=1;
+          bills['50'] -= 1;
+          bills['25'] -= 1;
           continue;
-        } else if (bills['25'] >=3) {
-          bills['25'] -=3;
+        } else if (bills['25'] >= 3) {
+          bills['25'] -= 3;
           continue;
         }
         return 'NO';
@@ -74,15 +74,34 @@ module.exports = {
     return 'YES';
   },
   diamond: (n) => {
-    if (n % 2 === 0 || n < 0)
-      return null
-    let result = ''
-    var middle = Math.round(n/2 + 0.6)
+    if (n % 2 === 0 || n < 0) {
+      return null;
+    }
+    let result = '';
+    const middle = Math.round(n / 2 + 0.6);
     
-    for (let i = -middle+1; i < middle; i++) {
-      let spaces = Math.abs(i);
-      let stars = n - Math.abs(i)*2;
-      result += `${' '.repeat(spaces)}${'*'.repeat(stars)}\n` 
+    for (let i = -middle + 1; i < middle; i++) {
+      const spaces = Math.abs(i);
+      const stars = n - Math.abs(i) * 2;
+      result += `${' '.repeat(spaces)}${'*'.repeat(stars)}\n`;
+    }
+    return result;
+  },
+  
+  likes: (n) => {
+    if (n.length === 0) {
+      return 'no one likes this';
+    }
+    let result = `${n[0]}`;
+    switch (n.length) {
+      case 1:
+        result += ' likes this'; break;
+      case 2:
+        result += ` and ${n[1]} like this`; break;
+      case 3:
+        result += `, ${n[1]} and ${n[2]} like this`; break;
+      default:
+        result += `, ${n[1]} and ${n.length - 2} others like this`; break;
     }
     return result;
   }
--- javascript/test/test.js
+++ javascript/test/test.js
@@ -20,34 +20,42 @@ describe('Codewars katas', () => {
   
   it('Is Isogram', () => {
     assert.equal(kata.isIsogram('ABCD'), true)
-    assert.equal(kata.isIsogram("Dermatoglyphics"), true );
-    assert.equal(kata.isIsogram("isogram"), true );
-    assert.equal(kata.isIsogram("aba"), false, "same chars may not be adjacent" );
-    assert.equal(kata.isIsogram("moOse"), false, "same chars may not be same case" );
-    assert.equal(kata.isIsogram("isIsogram"), false );
-    assert.equal(kata.isIsogram(""), true, "an empty string is a valid isogram" );
+    assert.equal(kata.isIsogram('Dermatoglyphics'), true );
+    assert.equal(kata.isIsogram('isogram'), true );
+    assert.equal(kata.isIsogram('aba'), false, 'same chars may not be adjacent' );
+    assert.equal(kata.isIsogram('moOse'), false, 'same chars may not be same case' );
+    assert.equal(kata.isIsogram('isIsogram'), false );
+    assert.equal(kata.isIsogram(''), true, 'an empty string is a valid isogram' );
   });
   
   it('Get Sum', () => {
-    assert.equal(-1, kata.GetSum(0, -1));
-    assert.equal( 1, kata.GetSum(0, 1));
-    assert.equal( 2, kata.GetSum(-1, 2));
-    assert.equal(-3, kata.GetSum(-2, -1));
-    assert.equal(-9, kata.GetSum(-2, -4));
+    assert.equal(-1, kata.getSum(0, -1));
+    assert.equal(1, kata.getSum(0, 1));
+    assert.equal(2, kata.getSum(-1, 2));
+    assert.equal(-3, kata.getSum(-2, -1));
+    assert.equal(-9, kata.getSum(-2, -4));
   });
   
   it('People In Line', () => {
-    assert.equal("YES", kata.tickets([25, 25, 50, 50]));
-    assert.equal("NO", kata.tickets([25, 100]));
-    assert.equal("YES", kata.tickets([25, 50, 25, 100]));
+    assert.equal('YES', kata.tickets([25, 25, 50, 50]));
+    assert.equal('NO', kata.tickets([25, 100]));
+    assert.equal('YES', kata.tickets([25, 50, 25, 100]));
   });
 
   it('Should print diamond', () => {
-    assert.equal(kata.diamond(3), " *\n***\n *\n")
-    assert.equal(kata.diamond(5), "  *\n ***\n*****\n ***\n  *\n")
-    assert.equal(kata.diamond(2), null)
-    assert.equal(kata.diamond(-3), null)
-    assert.equal(kata.diamond(0), null)
+    assert.equal(kata.diamond(3), ' *\n***\n *\n');
+    assert.equal(kata.diamond(5), '  *\n ***\n*****\n ***\n  *\n');
+    assert.equal(kata.diamond(2), null);
+    assert.equal(kata.diamond(-3), null);
+    assert.equal(kata.diamond(0), null);
+  });
+  
+  it('should return correct text', () => {
+    assert.equal(kata.likes([]), 'no one likes this');
+    assert.equal(kata.likes(['Peter']), 'Peter likes this');
+    assert.equal(kata.likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+    assert.equal(kata.likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+    assert.equal(kata.likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
   });
 });
 
